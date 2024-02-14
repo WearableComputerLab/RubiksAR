87942228223f2d8bfa25d16f6814c31c3df206f6
Alex Pepicelli <117417246+alexpepicelli@users.noreply.github.com>
2024-02-14 16:32:11 +1030

    Actually working

--- assets/Scripts/SimpleOutlet.cs
+++ assets/Scripts/SimpleOutlet.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 
 public class SimpleOutlet : MonoBehaviour
 {
-    public string streamName = "LSLUnity";
+    public string streamName = "UnityMarkers";
+    public string streamName2 = "UnityGaze";
     public string streamType = "Markers";
+    public string streamType2 = "Markers";
     private StreamOutlet _outlet;
+    private StreamOutlet _outlet2;
     private string[] _sample = {""};
+    private string[] _sample2 = { "" };
     
     // Start is called before the first frame update
     void Start()
@@ -20,11 +24,24 @@ public class SimpleOutlet : MonoBehaviour
         hash.Append(gameObject.GetInstanceID());
         StreamInfo streamInfo = new StreamInfo(streamName, streamType, 1, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_string, hash.ToString());
         _outlet = new StreamOutlet(streamInfo);
+
+        var hash2 = new Hash128();
+        hash2.Append(streamName2);
+        hash2.Append(streamType2);
+        hash2.Append(gameObject.GetInstanceID());
+        StreamInfo streamInfo2 = new StreamInfo(streamName2, streamType2, 1, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_string, hash.ToString());
+        _outlet2 = new StreamOutlet(streamInfo2);
     }
 
     public void SendMarker(string marker)
     {
         _sample[0] = marker;
         _outlet.push_sample(_sample);
+    }
+
+    public void SendGaze(string gaze)
+    {
+        _sample2[0] = gaze;
+        _outlet2.push_sample(_sample2);
     }
 }
